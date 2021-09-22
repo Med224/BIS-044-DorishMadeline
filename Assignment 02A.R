e64462c5b2869f6d32d0203bde7388559b8b58d9
Maddie Dorish <med224@lehigh.edu>
2021-09-22 07:46:40 -0400

    Created a cat command

---
+++ Assignment 02A.R
@@ -0,0 +1,3 @@
+# Maddie, there are x days until the end of the semester!
+Countdown = as.double (as.Date("2021-12-03") - (Sys.Date()))
+cat("Maddie, there are", Countdown, "days left in the semester!")
