ecfd29f3e64b6732c0473ecc05fb91b553c9e876
ctaylor221 <89597002+ctaylor221@users.noreply.github.com>
2021-10-04 19:21:56 -0400

    Assignment 02A graded

--- Assignment 02A.R
+++ Assignment 02A.R
@@ -1,3 +1,3 @@
-# Maddie, there are x days until the end of the semester! 50/59
+# Maddie, there are x days until the end of the semester! 50/50
 Countdown = as.double (as.Date("2021-12-03") - (Sys.Date()))
 cat("Maddie, there are", Countdown, "days left in the semester!")
